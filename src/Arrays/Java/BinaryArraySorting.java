3e2447343b882c4aacb3bbee6c92b88a58e6da87
shrinathjoshi <shrinathjoshi1997@gmail.com>
2020-07-29 00:52:05 +0530

    added question

--- src/Arrays/Java/BinaryArraySorting.java
+++ src/Arrays/Java/BinaryArraySorting.java
@@ -1,5 +1,43 @@
 package Arrays.Java;
 
+/*
+https://practice.geeksforgeeks.org/problems/binary-array-sorting5355/1/?track=amazon-arrays&batchId=192
+
+Binary Array Sorting
+Given a binary array A[] of size N. The task is to arrange the array in increasing order.
+
+Note: The binary array contains only 0  and 1.
+
+Input:
+The first line of input contains an integer T, denoting the test cases. Every test case contains two lines, the first line is N(size of the array) and the second line is space-separated elements of the array.
+
+Output:
+Space-separated elements of sorted arrays. There should be a new line between the output of every test case.
+
+Your Task:
+Complete the function SortBinaryArray() which takes given array as input and returns the sorted array.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Challenge: Try doing it in one pass.
+
+Constraints:
+1 < = T <= 100
+1 <= N <= 106
+0 <= A[i] <= 1
+
+Example:
+Input:
+2
+5
+1 0 1 1 0
+10
+1 0 1 1 1 1 1 0 0 0
+Output:
+0 0 1 1 1
+0 0 0 0 1 1 1 1 1 1
+ */
 public class BinaryArraySorting {
 	int[] sortBinaryArray(int arr[], int n) {
 		int pivot = -1;
